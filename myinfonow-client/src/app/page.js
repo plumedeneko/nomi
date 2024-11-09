8708223804ff39c4f0a8014910cd29100e131397
Teaapoo <dliu224@my.yorku.ca>
2024-11-09 16:10:34 -0500

    Co-authored-by: Joshua Huliganga <joshua-huliganga@users.noreply.github.com> Co-authored-by: plumedeneko <plumedeneko@users.noreply.github.com>

--- myinfonow-client/src/app/page.js
+++ myinfonow-client/src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
@@ -20,7 +21,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly. balls</li>
+          <li>Save and see your changes instantly. ballsssssssssss</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
